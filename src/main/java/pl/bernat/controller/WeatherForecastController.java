8de501fe1fef737640153c913d7a071c623ae59c
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2023-08-23 21:15:32 +0200

    display citySelectorWindow

--- src/main/java/pl/bernat/controller/WeatherForecastController.java
+++ src/main/java/pl/bernat/controller/WeatherForecastController.java
@@ -53,7 +53,7 @@ public class WeatherForecastController extends BaseController {
 
     @FXML
     void editMainButton() {
-
+        viewFactory.citySelectorWindow();
     }
     public void setAnchorPaneMargin(){
         HBox.setMargin(weatherForecastAnchorPane, new Insets(0, 20, 0,0));
--- src/main/java/pl/bernat/view/ViewFactory.java
+++ src/main/java/pl/bernat/view/ViewFactory.java
@@ -1,9 +1,11 @@
 package pl.bernat.view;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import pl.bernat.controller.BaseController;
+import pl.bernat.controller.CitySelectorController;
 import pl.bernat.controller.MainWindowController;
 import pl.bernat.controller.WeatherForecastController;
 
@@ -18,41 +20,60 @@ public class ViewFactory {
         this.citiesList = citiesList;
         activeStages = new ArrayList<Stage>();
     }
-    public void showMainWindow() throws IOException {
+    public void showMainWindow(){
         BaseController controller = new MainWindowController(citiesList, this, "/FXMLFiles/MainWindow.fxml");
+        initializeStage(controller);
+    }
+    public void citySelectorWindow(){
+        BaseController controller = new CitySelectorController(citiesList, this, "/FXMLFiles/CitySelectorWindow.fxml");
         initializeStage(controller);
     }
 
     private void initializeWeatherForecastWindows(FXMLLoader weatherForecastLoader, WeatherForecastController controller) throws IOException {
         int i=citiesList.size();
-
-        do {
-            weatherForecastLoader.load();
-            i--;
-            if(i >= 1){
-                controller.setAnchorPaneMargin();
-            }
-        } while (i > 0);
+try {
+    do {
+        weatherForecastLoader.load();
+        i--;
+        if (i >= 1) {
+            controller.setAnchorPaneMargin();
+        }
+    } while (i > 0);
+} catch (IOException e){
+    e.printStackTrace();
+}
     }
     private FXMLLoader setFxmlLoader(BaseController controller){
         FXMLLoader loader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
         loader.setController(controller);
         return loader;
     }
-    private void initializeStage(BaseController baseController) throws IOException {
-        FXMLLoader mainWindow = setFxmlLoader(baseController);
+    private void initializeStage(BaseController baseController){
+        FXMLLoader window = setFxmlLoader(baseController);
 
-        Scene scene = new Scene(mainWindow.load());
+        Parent parent;
+        try {
+            parent = window.load();
+            if(baseController.getClass() == MainWindowController.class){
+                setForecastWindows(window);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
 
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void setForecastWindows(FXMLLoader mainWindow) throws IOException {
         WeatherForecastController weatherController = new WeatherForecastController(citiesList, this, "/FXMLFiles/WeatherForecast.fxml");
         FXMLLoader weatherForecastLoader = setFxmlLoader(weatherController);
 
         weatherForecastLoader.setRoot(mainWindow.getNamespace().get("weatherForecastSpace"));
 
         initializeWeatherForecastWindows(weatherForecastLoader, weatherController);
-
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
     }
 }
